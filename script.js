4373707978720dfccb1a1b217fcb8a3a00e2fcb2
TanakaJenamiso <bida25-237@thuto.bac.ac.bw>
2026-05-23 00:05:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("JavaScript is connected!");
+ function showMessage() { document.getElementById("message").innerHTML = "Thank You for trusting Siamo Belle Bites, we will get back to You!"}
+        
