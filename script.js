bb871cb0ac6aa43f5a7e5d0750f851354ab1154c
TanakaJenamiso <bida25-237@thuto.bac.ac.bw>
2026-05-25 01:34:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-console.log("JavaScript is connected!");
- function showMessage() { document.getElementById("message").innerHTML = "Thank You for trusting Siamo Belle Bites, we will get back to You!"}
-        
